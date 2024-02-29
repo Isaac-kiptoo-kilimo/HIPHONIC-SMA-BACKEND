374ef90a77323258263a2c46f9c04606c9615b9b
mwaniki-Titus <mwanikititus1@gmail.com>
2024-02-29 04:52:46 -0800

    added photos crud operations

--- src/routes/photosRoutes.js
+++ src/routes/photosRoutes.js
@@ -1,12 +1,13 @@
 import { Router } from 'express';
-import { getPhotos, getPhotoById, createPhoto, deletePhoto } from '../controllers/photoController.js';
+import { getPhotos, createPhoto, deletePhoto} from '../controllers/photoController.js';
 import logger from '../utils/Logger.js';
 
 const photoRouter = Router();
 
-photoRouter.get('/photos', getPhotos);
-photoRouter.get('/photos/:id', getPhotoById);
+photoRouter.get('/photos/:UserID', getPhotos);
+// photoRouter.get('/photos/:id', getPhotoById);
 photoRouter.post('/photos', createPhoto);
 photoRouter.delete('/photos/:id', deletePhoto);
+// photoRouter.get("/photo/:UserID", getOnePhotoByUID)
 
 export default photoRouter;
